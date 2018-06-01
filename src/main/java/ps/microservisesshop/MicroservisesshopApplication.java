b35e3877dba7be345d71bbb15f6e50461945a992
paulstarbi <paulstarbi@gmail.com>
2018-06-01 14:21:37 +0200

    create docker compose with sql container configuration, add Product entity, extend .gitignore file, configure hibernate and datasource;

---
+++ src/main/java/ps/microservisesshop/MicroservisesshopApplication.java
@@ -0,0 +1,12 @@
+package ps.microservisesshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservisesshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservisesshopApplication.class, args);
+	}
+}
