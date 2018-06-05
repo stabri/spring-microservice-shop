3e01f844e4fcf9b63b5d74686d664c5457767518
paulstarbi <paulstarbi@gmail.com>
2018-06-05 13:58:49 +0200

    support for loadBalancing in catalogService (chose one of inventory servise) look ProductService findByCode, some issues with lombok extend entities for getters and setters;

--- catalog-service/src/main/java/ps/catalogservice/entities/Product.java
+++ catalog-service/src/main/java/ps/catalogservice/entities/Product.java
@@ -16,6 +16,54 @@ public class Product {
     private String name;
     private String description;
     private double price;
+    @Transient
+    private boolean inStock = true;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isInStock() {
+        return inStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        this.inStock = inStock;
+    }
 }
--- catalog-service/src/main/java/ps/catalogservice/services/ProductService.java
+++ catalog-service/src/main/java/ps/catalogservice/services/ProductService.java
@@ -1,9 +1,15 @@
 package ps.catalogservice.services;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 import ps.catalogservice.entities.Product;
+import ps.catalogservice.entities.ProductInventoryResponse;
 import ps.catalogservice.repositories.ProductRepository;
 
 import java.util.List;
@@ -14,17 +20,39 @@ import java.util.Optional;
 @Slf4j
 public class ProductService {
     private final ProductRepository productRepository;
+    private final RestTemplate restTemplate;
+    private final Logger log = LoggerFactory.getLogger(ProductService.class);
 
     @Autowired
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, RestTemplate restTemplate) {
         this.productRepository = productRepository;
+        this.restTemplate = restTemplate;
     }
+
+    public Optional<Product> findProductByCode(String code) {
+        Optional<Product> productOptional = productRepository.findByCode(code);
+        if(productOptional.isPresent()) {
+            log.info("Fetching inventory level for product_code: "+code);
+            ResponseEntity<ProductInventoryResponse> itemResponseEntity =
+                    restTemplate.getForEntity("http://inventory-service/api/inventory/{code}",
+                            ProductInventoryResponse.class,
+                            code);
+            if(itemResponseEntity.getStatusCode() == HttpStatus.OK) {
+                Integer quantity = itemResponseEntity.getBody().getAvailableQuantity();
+                log.info("Available quantity: "+quantity);
+                productOptional.get().setInStock(quantity> 0);
+            } else {
+                log.error("Unable to get inventory level for product_code: "+code +
+                        ", StatusCode: "+itemResponseEntity.getStatusCode());
+            }
+        }
+        return productOptional;
+    }
+
 
     public List<Product> findAllProducts() {
         return productRepository.findAll();
     }
-
-    public Optional<Product> findProductByCode(String code) {
-        return productRepository.findByCode(code);
-    }
 }
+
+
